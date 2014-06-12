e9337481bed4b212beddc36b03572fb3836acfda
Arpon Raksit <arpon.raksit@gmail.com>
2014-06-12 09:34:34 -0400

    unjustify text, allow line breaks after arrows in math, add post times and fix feed time data

--- js/j.js
+++ js/j.js
@@ -6,39 +6,31 @@
         $(this).find(".desc").toggle();
       });
     });
-    if (!$(document.body).hasClass("math")) {
-      Hyphenator.run();
-    } else {
-      MathJax.Hub.Config({
-        extensions: ["tex2jax.js"],
-        tex2jax: {
-          inlineMath: [["$", "$"]]
+    MathJax.Hub.Config({
+      extensions: ["tex2jax.js"],
+      tex2jax: {
+        inlineMath: [["$", "$"]]
+      },
+      jax: ["input/TeX", "output/HTML-CSS"],
+      "HTML-CSS": {
+        scale: 95,
+        availableFonts: [],
+        webFont: "Neo-Euler",
+        mtextFontInherit: true
+      },
+      TeX: {
+        equationNumbers: {
+          autoNumber: "AMS"
         },
-        jax: ["input/TeX", "output/HTML-CSS"],
-        "HTML-CSS": {
-          scale: 95,
-          availableFonts: [],
-          webFont: "Neo-Euler",
-          mtextFontInherit: true
-        },
-        TeX: {
-          equationNumbers: {
-            autoNumber: "AMS"
-          },
-          extensions: ["AMSmath.js", "AMSsymbols.js", "/js/xypic.js"],
-          Macros: {
-            opname: ["\\operatorname{\\text{#1}}", 1],
-            mathcal: ["\\mathscr{#1}", 1],
-            mathrm: ["\\text{#1}", 1],
-            iso: "\\mathrel{\\large\\simeq}",
-            qed: "~~\\diamondsuit"
-          }
+        extensions: ["AMSmath.js", "AMSsymbols.js", "/js/xypic.js"],
+        Macros: {
+          opname: ["\\operatorname{\\text{#1}}", 1],
+          mathcal: ["\\mathscr{#1}", 1],
+          mathrm: ["\\text{#1}", 1],
+          iso: "\\mathrel{\\large\\simeq}"
         }
-      });
-      MathJax.Hub.Register.StartupHook("End", function() {
-        Hyphenator.run();
-      });
-    }
+      }
+    });
   });
 
 }).call(this);
