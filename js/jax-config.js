30d1b0176e5e5630bd0fa6d366d5b3a881b14320
Arpon Raksit <arpon.raksit@gmail.com>
2016-01-31 22:40:18 +0000

    resurrection, almost complete

---
+++ js/jax-config.js
@@ -0,0 +1,33 @@
+MathJax.Hub.Config({
+    extensions: ["tex2jax.js"],
+    tex2jax: {
+        inlineMath: [["$", "$"]],
+    },
+    jax: ["input/TeX", "output/HTML-CSS"],
+    "HTML-CSS": {
+        imageFont: null,
+        webFont: "STIX-Web",
+        scale: 100,
+        linebreaks: {
+            automatic: true,
+            width: "75% container"
+        }
+    },
+    showMathMenu: false,
+    TeX: {
+        extensions: ["AMSmath.js", "AMSsymbols.js", "/js/xypic.js"],
+        TagSide: "left",
+        TagIndent: "0em",
+        Macros: {
+            opname: ["\\operatorname{#1}", 1],
+            mathcal: ["\\mathscr{#1}", 1],
+            iso: "\\simeq",
+            c: "\\colon",
+            ce: "\\mathrel{:=}",
+            l: "\\left",
+            r: "\\right",
+        }
+    }
+});
+
+MathJax.Ajax.loadComplete("/js/jax-config.js");
